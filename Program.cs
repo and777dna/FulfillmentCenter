f488968b23bf82d11f6e391e9de622c674f5ff1a
fff <70720317+and777dna@users.noreply.github.com>
2026-03-11 13:43:21 +0100

    DI container with options for DB is added and configured

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-using FulfillmentCenter;
 using FulfillmentCenter.Controllers;
 using FulfillmentCenter.Data;
 using FulfillmentCenter.Repositories.Implementations;
@@ -19,6 +18,7 @@ var app = builder.Build();
 
 var connectionString = builder.Configuration.GetConnectionString("ConnectionStrings");
 builder.Services.AddDbContext<FulfillmentCenDbContext>(options => options.UseSqlServer(connectionString));
+//DbContextOptions
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -50,25 +50,3 @@ builder.Services.AddSingleton<ProductsController, ProductsController>();
 builder.Services.AddSingleton<ShipmentsController, ShipmentsController>();
 
 builder.Services.AddSingleton<DbContext, FulfillmentCenDbContext>();//TODO: to check if this is okay, because i need to register interface first
-    
-
-var container = new Container();
-
-container.Register<IInventoryService, InventoryService>();
-container.Register<IOrderService, OrderService>();
-container.Register<IProductService, ProductService>();
-container.Register<IShipmentService, ShipmentService>();
-
-container.Register<IFulfillmentCenterRepository, SqlFulfillmentCenterRepository>();
-container.Register<IInventoryRepository, SqlInventoryRepository>();
-container.Register<IOrderItemRepository, SqlOrderItemRepository>();
-container.Register<IOrderRepository, SqlOrderRepository>();
-container.Register<IProductRepository, SqlProductRepository>();
-container.Register<IShipmentRepository, SqlShipmentRepository>();
-
-container.Register<InventoryController, InventoryController>();
-container.Register<OrdersController, OrdersController>();
-container.Register<ProductsController, ProductsController>();
-container.Register<ShipmentsController, ShipmentsController>();
-
-container.Register<FulfillmentCenDbContext, FulfillmentCenDbContext>();
