70af7276adc72e24d9257629e667aafe6a54333a
fff <70720317+and777dna@users.noreply.github.com>
2026-03-24 10:43:51 +0100

    ..

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using FulfillmentCenter.Services.Implementations;
 using FulfillmentCenter.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,10 +17,35 @@ builder.Services.AddSwaggerGen();
 
 
 var connectionString = builder.Configuration.GetConnectionString("FulfilmentCenterDatabase");
-builder.Services.AddDbContext<FulfillmentCenDbContext>(options => options.UseSqlServer(connectionString));
+//builder.Services.AddDbContext<FulfillmentCenDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDbContext<FulfillmentCenDbContext>(options => options.UseMySql(connectionString, 
+    ServerVersion.AutoDetect(connectionString)));
+//builder.Services.AddDbContext<FulfillmentCenDbContext>(options => options.UseSqlServer(connectionString));
 
 //builder.Services.AddControllers();
 
+builder.Services.AddTransient<IInventoryService, InventoryService>();
+builder.Services.AddTransient<IOrderService, OrderService>();
+builder.Services.AddTransient<IProductService, ProductService>();
+builder.Services.AddTransient<IFulfillmentCenterService, FulfillmentCenterService>();
+
+builder.Services.AddTransient<IShipmentService, ShipmentService>();
+
+builder.Services.AddTransient<IFulfillmentCenterRepository, SqlFulfillmentCenterRepository>();
+builder.Services.AddTransient<IInventoryRepository, SqlInventoryRepository>();
+builder.Services.AddTransient<IOrderItemRepository, SqlOrderItemRepository>();
+builder.Services.AddTransient<IOrderRepository, SqlOrderRepository>();
+builder.Services.AddTransient<IProductRepository, SqlProductRepository>();
+builder.Services.AddTransient<IShipmentRepository, SqlShipmentRepository>();
+
+builder.Services.AddTransient<InventoryController, InventoryController>();
+builder.Services.AddTransient<OrdersController, OrdersController>();
+builder.Services.AddTransient<ProductsController, ProductsController>();
+builder.Services.AddTransient<ShipmentsController, ShipmentsController>();
+//builder.Services.AddControllers();
+
+/*
 builder.Services.AddTransient<IInventoryService, InventoryService>();
 builder.Services.AddTransient<IOrderService, OrderService>();
 builder.Services.AddTransient<IProductService, ProductService>();
@@ -39,6 +65,7 @@ builder.Services.AddTransient<ProductsController, ProductsController>();
 builder.Services.AddTransient<ShipmentsController, ShipmentsController>();
 
 builder.Services.AddScoped<DbContext, FulfillmentCenDbContext>();//TODO: to check if this is okay, because i need to register interface first
+*/
 
 
 var app = builder.Build();
