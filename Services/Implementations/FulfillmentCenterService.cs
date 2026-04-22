472214c31b72098be41044c7ae37d56cfa114d2d
fff <70720317+and777dna@users.noreply.github.com>
2026-04-22 16:33:51 +0200

    demo "cache" for FulfillmentCenterService is implemented

--- Services/Implementations/FulfillmentCenterService.cs
+++ Services/Implementations/FulfillmentCenterService.cs
@@ -5,13 +5,27 @@ using FulfillmentCenter.Services.Interfaces;
 
 namespace FulfillmentCenter.Services.Implementations;
 
-public class FulfillmentCenterService(IFulfillmentCenterRepository fulfillmentCenterRepository) : IFulfillmentCenterService
+public class FulfillmentCenterService : IFulfillmentCenterService
 {
-    private IFulfillmentCenterRepository _fulfillmentCenterRepository = fulfillmentCenterRepository;
+    private IFulfillmentCenterRepository _fulfillmentCenterRepository;
+    private Lazy<Task<List<DistributionCenter>>> _distributionCenters;
+
+    public FulfillmentCenterService(IFulfillmentCenterRepository fulfillmentCenterRepository)
+    {
+        _fulfillmentCenterRepository = fulfillmentCenterRepository;
+        ResetCache();
+    }
+
+    private void ResetCache()
+    {
+        _distributionCenters = new Lazy<Task<List<DistributionCenter>>>(() => _fulfillmentCenterRepository.Read());
+    }
+    
     public async Task<DistributionCenter> FindFulfillmentCenter(Guid centerId)
-    {// 0f8fad5b-d9cb-469f-a165-70867728950e
-        var fulfillmentCenters = await _fulfillmentCenterRepository.Read();
-        var fulfillmentCenter = fulfillmentCenters.FirstOrDefault(center => center.Id == centerId);
+    {
+        var centers = await _distributionCenters.Value;
+        
+        var fulfillmentCenter = centers.FirstOrDefault(center => center.Id == centerId);
         if (fulfillmentCenter != null)
         {
             return fulfillmentCenter;
